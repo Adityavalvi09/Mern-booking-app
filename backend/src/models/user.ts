e63a0c6832aa1b043e84309a8fb41934b6e45f38
Aditya Valvi <adityavalvi8392@gmail.com>
2024-08-08 01:00:15 +0530

    added my hotels view and added e2e test

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,21 +1,30 @@
-import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
-import { UserType } from "../shared/types";
+import mongoose, { Document, Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+import { UserType } from '@shared/types';
 
-const userSchema = new mongoose.Schema({
+interface UserDocument extends UserType, Document {
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<UserDocument>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
 });
 
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 8);
+userSchema.pre<UserDocument>('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
   }
   next();
 });
 
-const User = mongoose.model<UserType>("User", userSchema);
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+  const user = this as UserDocument;
+  return bcrypt.compare(candidatePassword, user.password);
+};
 
+const User = mongoose.model<UserDocument>('User', userSchema);
 export default User;

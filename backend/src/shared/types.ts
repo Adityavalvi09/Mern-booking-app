8e95663ea31bf5bc553a71a6329d349220f6636f
Aditya Valvi <adityavalvi8392@gmail.com>
2024-08-08 00:17:38 +0530

    added my hotels view and added e2e test

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -1,16 +1,17 @@
-export type HotelType = {
-    _id : string;
-    userId: string;
-    name: string;
-    city:string;
-    country: string;
-    description : string;
-    type: string;
-    adultCount: number;
-    childCount: number;
-    facilities : string[];
-    pricePerNight: number;
-    starRating: number;
-    imageUrls: string[];
-    lastUpdated: Date;
+import { Document } from 'mongoose';
+
+export interface HotelType extends Document {
+  userId: string;
+  name: string;
+  city: string;
+  country: string;
+  description: string;
+  type: string;
+  adultCount: number;
+  childCount: number;
+  facilities: string[];
+  pricePerNight: number;
+  starRating: number;
+  imageUrls: string[];
+  lastUpdated: Date;
 }

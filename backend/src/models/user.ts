d1706aa96c96cef18a00dff1b8d94cdc3edc7c32
Aditya Valvi <adityavalvi8392@gmail.com>
2024-08-08 18:33:29 +0530

    added my hotels view and added e2e test

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,6 +1,6 @@
 import mongoose, { Document, Schema } from 'mongoose';
 import bcrypt from 'bcryptjs';
-import { UserType } from '@shared/types';
+import { UserType } from '../shared/types';
 
 interface UserDocument extends Omit<UserType, '_id'>, Document {
   comparePassword(candidatePassword: string): Promise<boolean>;
